d8a6b4177d07c375fee30ad89382f4223f0bd0e4
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2023-12-31 00:40:29 -0500

    Update Google_script.js 12.31.2024

--- Google_script.js
+++ Google_script.js
@@ -2,6 +2,6 @@ const spreadsheetId = '1fvD8eBUJNpJLP7EGWpY2qUjW1OKweThcMjfkxlSNkyg'
 fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json`)
     .then(res => res.text())
     .then(text => {
-        const json = JSON.parse(text.substr(47).slice(0, -2))
+        const json = JSON.parse(text.substring(47, text.length - 2));
 		console.log(text)
     })
